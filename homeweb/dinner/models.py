2825cdf10c562b2a3d98e4c10a61ea8ee37f0d53
Shawn Renner <shawn.renner@gmail.com>
2016-12-09 16:45:45 -0600

    initial models

--- homeweb/dinner/models.py
+++ homeweb/dinner/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=500)
+
+class Meal(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    ingredients = models.ManyToManyField(Ingredient)
+
+class MealHistory(models.Model):
+    meal = models.ForeignKey(Meal)
+    date = models.DateField()
+    comments = models.CharField(max_length=1000)
