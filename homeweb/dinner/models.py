38cd572fa3db8535f8cd30c8938ef34bbb8096e9
Shawn Renner <shawn.renner@gmail.com>
2016-12-10 15:17:41 -0600

    make date nullable

--- homeweb/dinner/models.py
+++ homeweb/dinner/models.py
@@ -11,7 +11,7 @@ class Meal(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=1000, blank=True)
     ingredients = models.ManyToManyField(Ingredient)
-    last_ate = models.DateField()
+    last_ate = models.DateField(null=True)
     def __str__(self):
         return self.name
 
