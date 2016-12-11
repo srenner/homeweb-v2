6c133502fef41148029831e051099dd7cf001cc1
Shawn Renner <shawn.renner@gmail.com>
2016-12-10 22:27:40 -0600

    add app for landing page etc.

--- homeweb/homeweb/urls.py
+++ homeweb/homeweb/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^', include('landing.urls')),
     url(r'^dinner/', include('dinner.urls')),
     url(r'^admin/', admin.site.urls),
 ]
