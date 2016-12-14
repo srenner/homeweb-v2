b54543050ff1db6ca84d347e57bb43b6744e037d
Shawn Renner <shawn.renner@gmail.com>
2016-12-13 20:45:17 -0600

    make shopping list

--- homeweb/dinner/static/dinner/js/index.js
+++ homeweb/dinner/static/dinner/js/index.js
@@ -29,6 +29,7 @@ var vm = new Vue({
     togglePlanningMode: function() {
       if(vm.mode === vm.modeEnum.PLANNING) {
         vm.mode = vm.modeEnum.NORMAL;
+        vm.shoppingList = [];
       }
       else {
         vm.mode = vm.modeEnum.PLANNING;
@@ -46,8 +47,37 @@ var vm = new Vue({
       }
       else {
         meal.selected = false;
+        vm.removeFromShoppingList(meal.ingredients);
       }
       Vue.set(vm.meals, index, meal);
+    },
+    toggleShoppingList: function(ingredient) {
+      var alreadyExists = false;
+      for(var i = 0; i < vm.shoppingList.length; i++) {
+        var shoppingItem = vm.shoppingList[i];
+        //todo maybe get meal_ingredient_id from server for the if-statement
+        if(shoppingItem.id === ingredient.id
+            && shoppingItem.meal_name === ingredient.meal_name) {
+          vm.shoppingList.splice(i,1);
+          alreadyExists = true;
+        }
+      }
+      if(!alreadyExists) {
+        vm.shoppingList.push(ingredient);
+      }
+    },
+    removeFromShoppingList: function(ingredients) {
+      for(var i = 0; i < ingredients.length; i++) {
+        var ingredient = ingredients[i];
+        for(var j = 0; j < vm.shoppingList.length; j++) {
+          var shoppingItem = vm.shoppingList[j];
+          //todo maybe get meal_ingredient_id from server for the if-statement
+          if(shoppingItem.id === ingredient.id
+              && shoppingItem.meal_name === ingredient.meal_name) {
+            vm.shoppingList.splice(j,1);
+          }
+        }
+      }
     },
     getMeals: function() {
       dinner_http.getMeals(getMeals_success, getMeals_failed);
