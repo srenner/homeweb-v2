a738ce157fc3a43f5a4dfb85e35a49a6be68f2d9
srenner <shawn.renner@gmail.com>
2016-12-20 21:23:47 -0600

    basic landing template

--- homeweb/homeweb/settings.py
+++ homeweb/homeweb/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
+    'landing.apps.LandingConfig',
     'dinner.apps.DinnerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
